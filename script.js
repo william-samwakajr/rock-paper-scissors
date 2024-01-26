e37ce92e5b5a2fec4d58000f14033bf6206a146e
william-samwakajr <williamsamwaka05@gmail.com>
2024-01-26 16:05:02 +0200

    adding scripts to game

--- script.js
+++ script.js
@@ -1,61 +1,14 @@
-let comp = Math.floor(Math.random() * 3);
-let ans = document.getElementById('compans');
-let message = document.getElementById('result');
-const rock = document.getElementById('rock');
-let paper = document.getElementById('paper');
-let scissors = document.getElementById('scissors');
-rock.addEventListener('click', function() {
-    let me = 1;
-    comp = Math.floor(Math.random() * 4);
-    if (me == comp) {
-        message.innerText = "Draw";
-    } else if (me == 1 && comp == 2) {
-        message.innerText = "You Lose";
-    } else if (me == 1 && comp == 3) {
-        message.innerText = "You Win";
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+let computerChoice = Math.floor(Math.random() * 3);
+
+class contestant {
+    constructor() {
+        this.points = 0;
+        this.winstatus = null;
     }
-    if (comp == 1) {
-        ans.innerText = " rock ";
-    } else if (comp == 2) {
-        ans.innerText = "paper";
-    } else if (comp == 3) {
-        ans.innerText = "scissors";
-    }
-});
-paper.addEventListener('click', function() {
-    let me = 2;
-    comp = Math.floor(Math.random() * 4);
-    if (me == comp) {
-        message.innerText = "Draw"
-    } else if (me == 2 && comp == 3) {
-        message.innerText = "You Lose"
-    } else if (me == 2 && comp == 1) {
-        message.innerText = "You Win"
-    }
-    if (comp == 1) {
-        ans.innerText = " rock ";
-    } else if (comp == 2) {
-        ans.innerText = "paper";
-    } else if (comp == 3) {
-        ans.innerText = "scissors";
-    }
-});
-scissors.addEventListener('click', function() {
-    let me = 3;
-    comp = Math.floor(Math.random() * 4);
-    if (me == comp) {
-        message.innerText = "Draw"
-    } else if (me == 3 && comp == 1) {
-        message.innerText = "You Lose"
-    } else if (me == 3 && comp == 2) {
-        message.innerText = "You Win"
-    }
-    if (comp == 1) {
-        ans.innerText = " rock ";
-    } else if (comp == 2) {
-        ans.innerText = "paper";
-    } else if (comp == 3) {
-        ans.innerText = "scissors";
-    }
-});
-//
+}
+
+const player = new contestant();
+const computer = new contestant();
